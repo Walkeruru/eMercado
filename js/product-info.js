761a5275a73548c69810b09c651f6fc7e0cab5c8
Santiago Olivera <104280661+Walkeruru@users.noreply.github.com>
2022-11-10 02:54:59 -0300

    cambios y mejoras visuales

--- js/product-info.js
+++ js/product-info.js
@@ -1,7 +1,6 @@
 fetch(`https://japceibal.github.io/emercado-api/products/${localStorage.getItem("producto")}.json`)
     .then(response => response.json())
     .then(data =>{ datosRecibidos = data;
-        console.log(datosRecibidos);
         showProductInfo();
         showComentarios();
     })
@@ -10,21 +9,10 @@ fetch(`https://japceibal.github.io/emercado-api/products/${localStorage.getItem(
         let contenedor = document.getElementById("container");
         contenedor.innerHTML =`
         <div class="row">
-        <h2 class="py-4 col-lg-10 align-self-center">${datosRecibidos.name}</h2>
-        <button class="btn btn-success col-lg-1 col-4 align-self-center" id="btnComprar" onclick="comprar()">Comprar</button>
+        <h1 class="py-2 mt-3 fw-bold col-lg-10 align-self-center">${datosRecibidos.name}</h1>
         </div>
-        <hr>
-        <h6 class="fw-bold">Precio</h6>
-        <p>${datosRecibidos.currency} ${datosRecibidos.cost}</p>
-        <h6 class="fw-bold">Descripción</h6>
-        <p>${datosRecibidos.description}</p>
-        <h6 class="fw-bold">Categoría</h6>
-        <p>${datosRecibidos.category}</p>
-        <h6 class="fw-bold">Cantidad de vendidos</h6>
-        <p>${datosRecibidos.soldCount}</p>
-        <h6 class="fw-bold">Imágenes ilustrativas</h6>
         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
+            <div class="carousel-inner rounded-3">
                 <div class="carousel-item active">
                     <img src="${datosRecibidos.images[0]}" class="d-block w-100" alt="...">
                 </div>
@@ -47,7 +35,60 @@ fetch(`https://japceibal.github.io/emercado-api/products/${localStorage.getItem(
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
+        <hr>
+        <p class="fs-2">${datosRecibidos.currency} ${datosRecibidos.cost}</p>
+        <div class="d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+                <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+            </svg><p class="ms-2">Envío a todo el país</p>
+        </div>
+        <div class="d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#198754" class="bi bi-arrow-return-left align-self-center" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+            </svg> <p class="ms-2"><span class="text-success">Devolución gratis</span><br>
+                <span class="text-muted">Tienes 30 días desde que lo recibes.</span>
+            </p>
+        </div>
+        <h6 class="fw-bold mt-2">Cantidad de vendidos</h6>
+        <div class="cantidadVendidos mt-3 shadow-sm" ><p>${datosRecibidos.soldCount}</p>
+        </div>
+        <div class="row">
+            <button class="btn btn-success shadow col-lg-12 col-11 ms-lg-0 ms-3 p-2 align-self-center mt-3" id="btnComprar" onclick="carritoBtn()">Comprar ahora</button>
+            <button class="btn btn-primary shadow col-lg-12 col-11 p-2 ms-lg-0 ms-3 align-self-center mt-lg-2 mt-2" id="btnagregarCarrito" onclick="comprar()">Agregar al carrito</button>
+        </div>
+        <hr>
+        <h2>Descripción</h2>
+        <p class="p-1">${datosRecibidos.description}</p>
+        <h6 class="fw-bold">Categoría</h6>
+        <p class="p-1">${datosRecibidos.category}</p>
+        <hr>
+        <div>
+            <h4 class="mb-2">Medios de pago</h4>
+            <div class="bg-success d-flex p-3 rounded align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFFFFF" class="bi bi-credit-card" viewBox="0 0 16 16">
+                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z"/>
+                    <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z"/>
+                  </svg> <span class="ms-2 text-white">¡Paga en hasta 12 cuotas sin interés!</span>
+            </div>
+            <p class="mt-4">Tarjetas de crédito <br>
+            <span class="text-muted">¡Cuotas sin interés con bancos seleccionados!</span></p>
+            <div class="d-flex">
+                <img src="https://http2.mlstatic.com/storage/logos-api-admin/a5f047d0-9be0-11ec-aad4-c3381f368aaf-m.svg" alt="Visa">
+                <img class="ms-4" src="https://http2.mlstatic.com/storage/logos-api-admin/b2c93a40-f3be-11eb-9984-b7076edb0bb7-m.svg" alt="American Express">
+                <img class="ms-4" src="https://http2.mlstatic.com/storage/logos-api-admin/d7e372a0-f39b-11eb-8e0d-6f4af49bf82e-m.svg" alt="Oca">
+                <img class="ms-4" src="https://http2.mlstatic.com/storage/logos-api-admin/aa2b8f70-5c85-11ec-ae75-df2bef173be2-m.svg"  alt="Mastercard"> 
+            </div>
+            <p class="mt-4">Efectivo</p>
+            <div class="d-flex">
+                <img src="https://http2.mlstatic.com/storage/logos-api-admin/8c527760-f39a-11eb-826e-6db365b9e0dd-m.svg" alt="Abitab">
+                <img class="ms-4" src="https://http2.mlstatic.com/storage/logos-api-admin/c2d9dbb0-f39b-11eb-9984-b7076edb0bb7-m.svg" alt="Redpagos">
+            </div>
+        </div>
         <span id="alerta"></span>`;
+}
+function carritoBtn(){
+    comprar();
+    window.location.href = "cart.html";
 }
 //Evento que agrega el producto al carrito de compra usando el localstorage
 function comprar(){
@@ -90,11 +131,11 @@ function showComentarios(){
                 for (let i=0; i<comentarios.length; i++){
                 contenedor.innerHTML +=`
     <div class="border">
-        <div class="pt-2 ps-4 d-flex flex-row" id="comentario${i}">
+        <div class="pt-2 ps-2 d-flex flex-row" id="comentario${i}">
             <p class="fw-bold">${comentarios[i].user}</p> 
-            <p>- ${comentarios[i].dateTime} -</p>
+            <p class="me-2">-${comentarios[i].dateTime}</p>
         </div>
-        <p class="ps-4">${comentarios[i].description}</p>
+        <p class="ps-2">${comentarios[i].description}</p>
     </div>
         `
         estrellas(i,comentarios[i].score);}
@@ -160,15 +201,39 @@ function productosRelacionados(){
     contenedor.innerHTML +=`
     <hr>
     <h6 class="fw-bold">Productos Relacionados</h6>
-    <div class="row row-cols-1 row-cols-md-4 g-4 pointer">
-        <div class=" pt-4" onclick="redireccionar(${datosRecibidos.relatedProducts[0].id})">
+    <div class="row row-cols-md-4 g-4 d-none d-md-flex">
+        <div class="col pt-4 pointer" onclick="redireccionar(${datosRecibidos.relatedProducts[0].id})">
             <img src="${datosRecibidos.relatedProducts[0].image}" class="card-img-top border" alt="...">
             <p>${datosRecibidos.relatedProducts[0].name}</p>
         </div>
-        <div class="col pt-4" onclick="redireccionar(${datosRecibidos.relatedProducts[1].id})">
+        <div class="col pt-4 pointer" onclick="redireccionar(${datosRecibidos.relatedProducts[1].id})">
             <img src="${datosRecibidos.relatedProducts[1].image}" class="card-img-top border" alt="...">
             <p>${datosRecibidos.relatedProducts[1].name}</p>
         </div>
+    </div>
+    <!-----Carousel para celulares ----!>
+    <div class="row d-block d-md-none">
+        <div id="carouselProductos" class="carousel slide col-lg-4" data-bs-ride="carousel">
+            <div class="carousel-inner pointer">
+                <div class="carousel-item active" onclick="redireccionar(${datosRecibidos.relatedProducts[0].id})">
+                    <img src="${datosRecibidos.relatedProducts[0].image}" class="card-img-top border" alt="...">
+                    <p>${datosRecibidos.relatedProducts[0].name}</p>
+                </div>
+                <div class="carousel-item" onclick="redireccionar(${datosRecibidos.relatedProducts[1].id})">
+                    <img src="${datosRecibidos.relatedProducts[1].image}" class="card-img-top border" alt="...">
+                    <p>${datosRecibidos.relatedProducts[1].name}</p>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselProductos" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselProductos" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+        </div>
+    </div>
     `
 }
 function redireccionar(producto){
