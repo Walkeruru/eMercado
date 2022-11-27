cb8a70c2c62c6eb893dcee6d8dd76d528a876040
Santiago Olivera <santiagoolivera.soly1@gmail.com>
2022-11-27 18:33:09 -0300

    cambios visuales

--- js/cart.js
+++ js/cart.js
@@ -9,36 +9,12 @@ getJSONData(CART_INFO_URL+'25801.json')
   </svg>Carrito de compras</h2>
     <hr>
     <h4>Articulos a comprar</h4>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col"> </th>
-      <th scope="col" class="d-none d-md-table-cell">Nombre</th>
-      <th scope="col">Costo</th>
-      <th scope="col">Cantidad</th>
-      <th scope="col">Subtotal</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody id="productoListado">
-    <tr data-producto = "0">
-      <th scope="row"><img src="${datos.image}" alt="Producto" width="70px"></th>
-      <td class="d-none d-md-table-cell">${datos.name}</td>
-      <td class="costo">${datos.currency+" "+datos.unitCost}</td>
-      <td><input type="number" value="1" class="form-control cantidad" min="1"></td>
-      <td class="fw-bold subtotal">${datos.currency+" "+datos.unitCost}</td>
-      <td><button type="button" class="btn btn-outline-danger borrar" data-boton = "0">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-</svg>
-      </button></td>
-    </tr>
-  </tbody>
-</table>
+    <form method="post" action="http://localhost:3000/compra" class=" needs-validation" novalidate>
+    <input class="d-none" name="user" value=${localStorage.getItem('displayName') ||localStorage.getItem('userLogin')}>
+    <div class="row" id="productoListado">
+    </div>
 <hr>
 <h4 class="mt-4">Tipo de envío</h4>
-<form class=" needs-validation" novalidate>
 <div class="form-check mt-3">
   <input class="form-check-input porcentajes" type="radio" name="radioPorcentaje" id="flexRadioDefault1" value="0.15" checked required>
   <label class="form-check-label" for="flexRadioDefault1">
@@ -64,14 +40,14 @@ Selecciona un tipo de envío
 <div class="mb-3 row">
   <div class="col-6">
   <label for="calle" class="form-label">Calle  </label>
-  <input type="text" class="form-control col" id="calle" required>
+  <input type="text" class="form-control col" id="calle" name="calle" required>
       <div class="invalid-feedback">
         Ingresa una calle
       </div>
   </div>
   <div class="col-3 col-lg-3 col-5">
   <label for="numero" class="form-label">Numero </label>
-  <input type="text" class="form-control col" id="numero" required>
+  <input type="text" class="form-control col" id="numero" name="numero" required>
   <div class="invalid-feedback">
         Ingresa un número
       </div>
@@ -80,7 +56,7 @@ Selecciona un tipo de envío
 <div class="row">
   <div class="col">
   <label for="esquina" class="form-label">Esquina </label>
-  <input type="text" class="form-control" id="esquina" required>
+  <input type="text" class="form-control" id="esquina" name="esquina" required>
       <div class="invalid-feedback">
         Ingresa una esquina
       </div>
@@ -88,7 +64,6 @@ Selecciona un tipo de envío
   <div class="col">
   </div>
 </div>
-</form>
 <hr>
 <h4 class="mt-4">Costos</h4>
 <div class="list-group-item list-group-item-action mt-3">
@@ -145,32 +120,31 @@ Selecciona un tipo de envío
       </div>
       <div class="modal-body">
       <div class="container">
-      <form class="needs-validation" novalidate>
         <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="credito" value="credito" required>
+          <input class="form-check-input" type="radio" name="formaDePago" id="credito" value="credito" required>  
           <label class="form-check-label" for="credito">
           Tarjeta de crédito
-          </label>
+          </label>     
         </div>
         <hr>
         <div class="row">
           <div class="col-6">
             <label for="nroTarjeta" class="form-label">Número de tarjeta</label>
-            <input type="text" class="form-control" id="nroTarjeta" required>
+            <input type="text" class="form-control" id="nroTarjeta" name="nroTarjeta" required>
           </div>
           <div class="col-5">
             <label for="codTarjeta" class="form-label">Código de seg.</label>
-            <input type="text" class="form-control" id="codTarjeta" required>
+            <input type="text" class="form-control" id="codTarjeta" name="codTarjeta" required>
           </div>
         </div>
         <div class="row">
           <div class="col-6 mt-3">
             <label for="vencimiento" class="form-label">Vencimiento (MM/AA)</label>
-            <input type="text" class="form-control" id="vencimiento" required>
+            <input type="text" class="form-control" id="vencimiento" name="vencimiento" required>
           </div>
         </div>
         <div class="form-check mt-4">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="banco" value="banco" required>
+          <input class="form-check-input" type="radio" name="formaDePago" id="banco" value="banco" required>
           <label class="form-check-label" for="banco">
             Transferencia Bancaria
           </label>
@@ -179,18 +153,18 @@ Selecciona un tipo de envío
         <div class="row">
           <div class="col-6">
             <label for="nmoBanco" class="form-label">Número de cuenta</label>
-            <input type="text" class="form-control" id="nmoBanco" required>
+            <input type="text" class="form-control" id="nmoBanco" name="nroBanco" required>
           </div>
         </div>
       </div>
     </div>
-    </form>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cerrar</button>
       </div>
     </div>
   </div>
 </div>
+</form>
     `;
     display();
     //evento para cambiar el subtotal al aumentar o disminuir la cantidad
@@ -201,8 +175,7 @@ Selecciona un tipo de envío
       inputs[i].addEventListener("change",()=>{
         if(inputs[i].value>0){
           inputs[i].setAttribute("value",inputs[i].value);
-          console.log(costo[i].innerHTML.split(" ")[1])
-          subtotal[i].innerHTML=`${costo[i].innerHTML.split(" ")[0]+" "+Number(costo[i].innerHTML.split(" ")[1])*inputs[i].value}`
+          subtotal[i].innerHTML=`Subtotal: ${costo[i].innerHTML.split(" ")[1]+" "+Number(costo[i].innerHTML.split(" ")[2])*inputs[i].value}`
           calcularCosto();
           costoEnvio();
           pagoTotal();
@@ -224,25 +197,24 @@ Selecciona un tipo de envío
     botonesForm();
     document.getElementById("finalizar").addEventListener("click",(e)=>{
       let form = document.querySelector("form");
-      let formModal = document.querySelectorAll("form")[1];
       let avisoPago= document.getElementById("pagoNoSeleccionado");
       let alerta = document.querySelector(".alert");
-      if (form.checkValidity() && formModal.checkValidity() && inputsValidacion()) {
+      if (form.checkValidity()) {
+        //pausa el post al servidor;
+        e.preventDefault();
+        e.stopPropagation();
         form.classList.remove('was-validated');
-        formModal.classList.remove('was-validated');
         avisoPago.innerHTML=' ';
         alerta.classList.add('show');
         inputs.forEach(input=>{
           input.style.borderColor = '#ced4da';
         })
       }else {
+        e.preventDefault();
+        e.stopPropagation();
         form.classList.add('was-validated');
-        if(!formModal.checkValidity()){
-          formModal.classList.add('was-validated');
+        if(!document.getElementById('banco').checkValidity() && !document.getElementById('credito').checkValidity()){
           avisoPago.innerHTML='Debe seleccionar una forma de pago'
-        }
-        if(!inputsValidacion()){
-          inputsValidacion();
         }
         alerta.classList.remove('show');
       }
@@ -252,8 +224,10 @@ Selecciona un tipo de envío
      pagoTotal();
   });
 function botonesForm(){
+  let avisoPago= document.getElementById("pagoNoSeleccionado");
   //inhabilita los campos del radio no seleccionado en el modal
   document.getElementById("credito").addEventListener("click",()=>{
+    avisoPago.innerHTML='';
     document.getElementById("vencimiento").disabled = false;
     document.getElementById("codTarjeta").disabled = false;
     document.getElementById("nroTarjeta").disabled = false;
@@ -261,6 +235,7 @@ function botonesForm(){
     document.getElementById("metodoPago").innerHTML='Tarjeta de crédito';
   });
   document.getElementById("banco").addEventListener("click",()=>{
+    avisoPago.innerHTML='';
     document.getElementById("nmoBanco").disabled = false;
     document.getElementById("vencimiento").disabled = true;
     document.getElementById("codTarjeta").disabled = true;
@@ -272,9 +247,9 @@ function calcularCosto(){
   let subtotal = document.querySelectorAll(".subtotal");
   let total=0;
   for(let producto of subtotal){
-    if(producto.innerHTML.split(" ")[0] != 'USD'){
-     total += Number(producto.innerHTML.split(" ")[1]/42)
-    }else total += Number(producto.innerHTML.split(" ")[1])
+    if(producto.innerHTML.split(" ")[1] != 'USD'){
+     total += Number(producto.innerHTML.split(" ")[2]/42)
+    }else total += Number(producto.innerHTML.split(" ")[2])
   }
   return document.getElementById("costoTotal").innerHTML = `USD ${total.toFixed(2)}`;
 }
@@ -295,43 +270,38 @@ function pagoTotal(){
   let total = Number(subtotal)+Number(envio);
   return document.getElementById("pagoTotal").innerHTML = `USD ${total.toFixed(2)}`;
 }
-function inputsValidacion(){
-  let cantidades = document.querySelectorAll('.cantidad');
-  let malInput;
-  for(let i=0; i<cantidades.length; i++){
-    if(!cantidades[i].checkValidity()){
-      cantidades[i].style.borderColor = "#dc3545";
-       malInput = true
-    }
-    if(cantidades[i].checkValidity()){
-      cantidades[i].style.borderColor = '#ced4da';
-  }
-}
-  if(malInput){
-    return false;
-  }else return true
-}
+
 function display(){
-  
+
   if(localStorage.getItem("carrito")){
     let carrito = JSON.parse(localStorage.getItem("carrito"));
     let listado = document.getElementById("productoListado");
     listado.innerHTML='';
     for(let producto of carrito){
       listado.innerHTML += `
-    <tr data-producto =${carrito.indexOf(producto)+1}>
-      <th scope="row"><img src="${producto.image}" alt="Producto" width="70px"></th>
-      <td class="d-none d-md-table-cell">${producto.name}</td>
-      <td class="costo">${producto.currency+" "+producto.unitCost}</td>
-      <td><input type="number" value="1" class="form-control cantidad" min="1"></td>
-      <td class="fw-bold subtotal">${producto.currency+" "+producto.unitCost}</td>
-      <td><button type="button" class="btn btn-outline-danger borrar" data-boton =${carrito.indexOf(producto)+1}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-</svg>
-    </button></td>
-    </tr>
+      <div class="col col-md-6">
+      <div class="card mb-3" style="max-width: 540px;" data-producto =${carrito.indexOf(producto)+1}>
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${producto.image}" class="card-img-top mt-4 ms-md-2 rounded-3 border border-dark" alt="producto">
+          </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h4 class="card-title fw-bold">${producto.name}</h4>
+        <p class="card-text costo">Costo: ${producto.currency+" "+producto.unitCost}</p>
+        <input type="number" value="1" class="form-control cantidad" name="cantidadItem" min="1">
+        <p class="fw-bold subtotal mt-2">Subtotal: ${producto.currency+" "+producto.unitCost} </p>
+        <button type="button" class="btn btn-outline-danger borrar" data-boton =${carrito.indexOf(producto)+1}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+  </svg>
+      </button>
+        <input class="d-none" name="producto" value=${producto.name.split(" ").join("-")}
+      </div>
+      </div>
+      </div>
+    </div>
       `;
     }
     let botones=document.querySelectorAll('button[data-boton]');
@@ -350,4 +320,4 @@ function display(){
     localStorage.setItem("carrito",JSON.stringify(auto));
     display();
   }
-}
+};
